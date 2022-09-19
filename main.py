691a7c472300602fdd09bdee552492e2f0c07ae9
yamsosinsky <yamsosinsky@gmail.com>
2022-09-19 15:23:15 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import pygame
+import const
+import mainField
+import solider
+import Screen
+
+finish = False
+while not finish:
+    Screen.grass_screen2()
+    Screen.move_solider()
+    solider.move()
+    if solider.step_on_bomb():
+        Screen.losser()
+        Screen.loss()
+    if solider.step_on_flag():
+        Screen.win()
+    if
+
 
