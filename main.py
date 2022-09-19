b2dc661155c1040000d71aeea05d0d32da9ff4f6
yamsosinsky <yamsosinsky@gmail.com>
2022-09-19 15:40:50 +0300

    j

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ import solider
 import Screen
 
 finish = False
+screen = pygame.display.set_mode(const.size)
+mainField.solider_matrix()
 while not finish:
-    Screen.grass_screen2()
-    Screen.move_solider()
-    solider.move()
+    Screen.grass_screen2(screen)
+
+    solider.move(screen)
     if solider.step_on_bomb():
-        Screen.losser()
-        Screen.loss()
+        Screen.losser(screen)
+        Screen.loss(screen)
+        finish = True
     if solider.step_on_flag():
-        Screen.win()
+        Screen.win(screen)
+        finish = True
+    if mainField.want_to_show_boom():
+        Screen.grid_screen(screen)
+
 
 
 
